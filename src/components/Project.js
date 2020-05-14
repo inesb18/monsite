86ba2c016ce7bab64c539714cd49618c0aea5f71
Ines Belghiti <ines.belghiti@gmail.com>
2020-05-14 17:01:36 -0400

    Made the project items responsive (touch devices)

---
+++ src/components/Project.js
@@ -0,0 +1,168 @@
+import React, { useState, useRef, useEffect } from "react";
+import styled from 'styled-components';
+
+const StyledProject = styled.div`
+  background-image: url(${props => props.image});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  .infos {
+    opacity: 0;
+    transition: all 0.1s;
+    padding: 2rem 1rem;
+    display: grid;
+    align-items: center;
+    grid-template-rows: auto 1fr 1fr 1fr;
+    grid-gap: 1rem;
+    justify-content: center;
+    text-align: center;
+    h2 {
+      font-family: "Philosopher";
+      font-size: 3.2rem;
+      margin: 0;
+      font-weight: bold;
+    }
+    p {
+      margin: 0;
+    }
+    a {
+      font-weight: bold;
+      margin: 0 auto;
+      width: fit-content;
+      padding: 1rem;
+      color: ${props => props.theme.peach};
+      border-top: 4px solid ${props => props.theme.peach};
+      border-bottom: 4px solid ${props => props.theme.peach};
+    }
+    ul {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+    }
+  }
+  &.visible {
+    background: white;
+    background: linear-gradient(rgba(255, 255, 255, 0.85), rgba(255, 255, 255, 0.85)), url(${props => props.image});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;  background-size: cover;
+    .infos {
+      transition: all 0.8s;
+      opacity: 1;
+    }
+  }
+  @media (max-width: 768px) {
+    .infos {
+      font-size: 1.4rem;
+      padding: 1rem;
+      grid-gap: 0.6rem;
+    }
+  }
+`;
+
+
+
+
+// class Submenu extends React.Component {
+//   render() {
+//     return (
+//       <ul className="nav__submenu">
+//         <li className="nav__submenu-item">
+//           <Link to="/patrimoine/visites">
+//             Visites
+//           </Link>
+//         </li>
+//         <li className="nav__submenu-item">
+//           <Link to="/patrimoine/artistes">
+//             Artistes
+//           </Link>
+//         </li>
+//       </ul>
+//     )
+//   }
+// }
+
+function useInfoVisible(initialIsVisible) {
+  const [isInfoVisible, setInfoVisible] = useState(initialIsVisible);
+  const ref = useRef(null);
+
+  const handleHideDropdown = (event) => {
+    if (event.key === "Escape") {
+      setInfoVisible(false);
+    }
+  };
+
+  const handleClickOutside = event => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setInfoVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleHideDropdown, true);
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("keydown", handleHideDropdown, true);
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  });
+
+  return { ref, isInfoVisible, setInfoVisible };
+}
+
+
+const Project = ({ project }) => {
+  const {
+    ref,
+    isInfoVisible,
+    setInfoVisible
+  } = useInfoVisible(false);
+
+  const showInfo = () => {
+    setInfoVisible(true);
+  };
+
+  const hideInfo = () => {
+    setInfoVisible(false);
+  };
+
+  const toggleInfo = () => {
+    setInfoVisible(!isInfoVisible);
+  };
+
+  return (
+    <StyledProject image={project.image} ref={ref}
+      onClick={toggleInfo}
+      onMouseEnter={showInfo}
+      onMouseLeave={hideInfo}
+      className={ isInfoVisible ? "visible" : ""}
+    >
+      <div className="infos">
+        <h2>{project.name}</h2>
+        <p>{project.description}</p>
+        <a href={project.URL} target="_blank" rel="noopener noreferrer">SITE WEB</a>
+        <ul>
+          {project.roles.map((role)=> {
+            return(<li key={role}>{role}</li>);
+          })}
+        </ul>
+      </div>
+    </StyledProject>
+  )
+}
+
+export default Project
+
+// <li className="navbar__section"
+//     ref={ref}
+//     onClick={toggleSubmenu}
+//     onMouseEnter={showSubmenu}'"'
+//     onMouseLeave={hideSubmenu}
+// >
+//   <a
+//     className={ (section === "Artistes" || section === "Visites") ? "navbar__section--name active" : "navbar__section--name"}
+//   >
+//     Patrimoine
+//   </a>
+//   { isSubmenuOpen &&  <Submenu />}
+// </li>
--- src/templates/projets-tech.js
+++ src/templates/projets-tech.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 import Page from "../components/Page";
 import PageTitle from "../components/PageTitle";
+import Project from "../components/Project";
 
 const StyledProjectsGrid = styled.div`
   display: grid;
@@ -16,67 +17,6 @@ const StyledProjectsGrid = styled.div`
   }
 `;
 
-const StyledProject = styled.div`
-  background-image: url(${props => props.image});
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center center;
-  .infos {
-    opacity: 0;
-    transition: all 0.1s;
-    padding: 2rem 1rem;
-    display: grid;
-    align-items: center;
-    grid-template-rows: auto 1fr 1fr 1fr;
-    grid-gap: 1rem;
-    justify-content: center;
-    text-align: center;
-    h2 {
-      font-family: "Philosopher";
-      font-size: 3.2rem;
-      margin: 0;
-      font-weight: bold;
-    }
-    p {
-      margin: 0;
-    }
-    a {
-      font-weight: bold;
-      margin: 0 auto;
-      width: fit-content;
-      padding: 1rem;
-      color: ${props => props.theme.peach};
-      border-top: 4px solid ${props => props.theme.peach};
-      border-bottom: 4px solid ${props => props.theme.peach};
-    }
-    ul {
-      list-style: none;
-      padding: 0;
-      margin: 0;
-    }
-  }
-  &:hover {
-    background: white;
-    background: linear-gradient(rgba(255, 255, 255, 0.85), rgba(255, 255, 255, 0.85)), url(${props => props.image});
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center center;  background-size: cover;
-    .infos {
-      transition: all 0.8s;
-      opacity: 1;
-    }
-  }
-  @media (max-width: 768px) {
-    .infos {
-      font-size: 1.4rem;
-      padding: 1rem;
-      grid-gap: 0.6rem;
-    }
-  }
-`;
-
-
-
 const Projects = ({ data }) => {
   const projects = data.projects.edges[0].node.frontmatter.projects;
   return (
@@ -85,18 +25,7 @@ const Projects = ({ data }) => {
       <StyledProjectsGrid>
         {projects.map((project)=> {
           return (
-            <StyledProject key={project.name} image={project.image}>
-              <div className="infos">
-                <h2>{project.name}</h2>
-                <p>{project.description}</p>
-                <a href={project.URL} target="_blank" rel="noopener noreferrer">SITE WEB</a>
-                <ul>
-                  {project.roles.map((role)=> {
-                    return(<li key={role}>{role}</li>);
-                  })}
-                </ul>
-              </div>
-            </StyledProject>
+            <Project key={project.name} project={project}/>
           )
         })}
 
