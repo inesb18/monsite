5f5221bfe5c512a313711e7e2a5930d58b9e9a22
Ines Belghiti <ines.belghiti@gmail.com>
2020-05-14 08:36:59 -0400

    Added animation on hover of navbar items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,6 +21,21 @@ const StyledListMenu = styled.ul `
     .active {
       color: ${props => props.theme.darkPeach};
     }
+    a:not(.active) {
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      background-image: linear-gradient(
+          to right,
+          ${props => props.theme.darkPeach},
+          ${props => props.theme.darkPeach} 50%,
+          ${props => props.theme.black} 50%);
+          background-size: 200% 100%;
+          background-position: 100%;
+    }
+    a:not(.active):hover {
+      transition: all 0.3s cubic-bezier(0.000, 0.000, 0.230, 1);
+      background-position: 0%;
+    }
   }
   @media (max-width: 768px) {
     display: none;
