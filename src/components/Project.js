53b3f04531915682104429b70f96a062d5830be7
Ines Belghiti <ines.belghiti@gmail.com>
2020-05-29 17:51:31 -0400

    Modified hover/click on project cards so that it is specific to devices with hover, and devices without hover

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import styled from 'styled-components';
 
 const StyledProject = styled.div`
@@ -8,6 +8,7 @@ const StyledProject = styled.div`
   background-position: center center;
   .infos {
     opacity: 0;
+    pointer-events: none;
     transition: all 0.1s;
     padding: 2rem 1rem;
     display: grid;
@@ -40,17 +41,6 @@ const StyledProject = styled.div`
       margin: 0;
     }
   }
-  &.visible {
-    background: white;
-    background: linear-gradient(rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.9)), url(${props => props.image});
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center center;  background-size: cover;
-    .infos {
-      transition: all 0.8s;
-      opacity: 1;
-    }
-  }
   @media (max-width: 768px) {
     .infos {
       font-size: 1.4rem;
@@ -58,64 +48,70 @@ const StyledProject = styled.div`
       grid-gap: 0.6rem;
     }
   }
+  @media (hover: hover) {
+    &.visibleFromHover {
+      background: white;
+      background: linear-gradient(rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.9)), url(${props => props.image});
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center center;  background-size: cover;
+      .infos {
+        pointer-events: auto;
+        transition: all 0.8s;
+        opacity: 1;
+      }
+    }
+  }
+  @media (hover: none) {
+    &.visibleFromClick {
+      background: white;
+      background: linear-gradient(rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.9)), url(${props => props.image});
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center center;  background-size: cover;
+      .infos {
+        pointer-events: auto;
+        transition: all 0.8s;
+        opacity: 1;
+      }
+    }
+  }
 `;
 
-function useInfoVisible(initialIsVisible) {
-  const [isInfoVisible, setInfoVisible] = useState(initialIsVisible);
-  const ref = useRef(null);
-
-  const handleHideDropdown = (event) => {
-    if (event.key === "Escape") {
-      setInfoVisible(false);
-    }
-  };
-
-  const handleClickOutside = event => {
-    if (ref.current && !ref.current.contains(event.target)) {
-      setInfoVisible(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("keydown", handleHideDropdown, true);
-    document.addEventListener("click", handleClickOutside, true);
-    return () => {
-      document.removeEventListener("keydown", handleHideDropdown, true);
-      document.removeEventListener("click", handleClickOutside, true);
-    };
-  });
-
-  return { ref, isInfoVisible, setInfoVisible };
-}
-
-
 const Project = ({ project }) => {
-  const {
-    ref,
-    isInfoVisible,
-    setInfoVisible
-  } = useInfoVisible(false);
+  const [isInfoVisibleFromHover, setInfoVisibleFromHover] = useState(false);
 
-  const showInfo = () => {
-    setInfoVisible(true);
+  const showInfoFromHover = () => {
+    setInfoVisibleFromHover(true);
   };
 
-  const hideInfo = () => {
-    setInfoVisible(false);
+  const hideInfoFromHover = () => {
+    setInfoVisibleFromHover(false);
   };
 
-  const toggleInfo = () => {
-    setInfoVisible(!isInfoVisible);
+  const [isInfoVisibleFromClick, setInfoVisibleFromClick] = useState(false);
+
+  const toggleInfoFromClick = () => {
+    setInfoVisibleFromClick(!isInfoVisibleFromClick);
   };
 
-  // const optimizedImage = project.image[0].replace(project.image[0].match(/upload\/(.+)\/MonSite/)[1],`q_auto,f_auto,c_fit,w_800`);
+  const classes = () => {
+    let classes = "";
+    if (isInfoVisibleFromClick) {
+      classes += "visibleFromClick";
+    }
+    if (isInfoVisibleFromHover) {
+      classes += " visibleFromHover";
+    }
+    return classes;
+  }
 
   return (
-    <StyledProject image={project.image} ref={ref}
-      onClick={toggleInfo}
-      onMouseEnter={showInfo}
-      onMouseLeave={hideInfo}
-      className={ isInfoVisible ? "visible" : ""}
+    <StyledProject image={project.image}
+      onClick={toggleInfoFromClick}
+      onMouseEnter={showInfoFromHover}
+      onMouseLeave={hideInfoFromHover}
+      className={classes()}
     >
       <div className="infos">
         <h2>{project.name}</h2>
